4d5f5916c694d7d62b3179539d2515e4e2257b38
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-12-17 12:00:21 +0700

    added 404 error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<section id="404" class="min-h-screen">
+	<div class="grid gap-2 text-center">
+		<p class="text-primary font-bold text-7xl">404</p>
+		<p class="font-bold text-3xl">Page Not Found</p>
+    <p>The requested URL was not found on this server.</p>
+	</div>
+</section>
